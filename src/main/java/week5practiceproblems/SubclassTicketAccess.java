69e6a526b01cdf1aa78f68a157a59729778e9f25
abhradip1020 <abhradip1020@gmail.com>
2026-09-11 18:36:49 +0530

    Add Week 5 practice problem 2

---
+++ src/main/java/week5practiceproblems/SubclassTicketAccess.java
@@ -0,0 +1,65 @@
+class PremiumMovieTicket extends MovieTicket {
+
+    PremiumMovieTicket(String seatNumber, String screenId,
+                       double ticketPrice, String movieTitle) {
+        super(seatNumber, screenId, ticketPrice, movieTitle);
+    }
+
+    void accessTicketPrice() {
+        System.out.println(ticketPrice);
+    }
+}
+
+public class SubclassTicketAccess {
+
+    static String classifyAccess(String fieldModifier,
+                                 String accessorContext) {
+
+        if (fieldModifier.equals("private")) {
+            if (accessorContext.equals("SAME_CLASS")) {
+                return "ALLOWED";
+            }
+            return "DENIED";
+        }
+
+        if (fieldModifier.equals("default")) {
+            if (accessorContext.equals("SAME_CLASS") ||
+                accessorContext.equals("SAME_PACKAGE")) {
+                return "ALLOWED";
+            }
+            return "DENIED";
+        }
+
+        if (fieldModifier.equals("protected")) {
+            if (accessorContext.equals("SAME_CLASS") ||
+                accessorContext.equals("SAME_PACKAGE") ||
+                accessorContext.equals("SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE")) {
+                return "ALLOWED";
+            }
+            return "DENIED";
+        }
+
+        if (fieldModifier.equals("public")) {
+            return "ALLOWED";
+        }
+
+        return "DENIED";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+            classifyAccess(
+                "protected",
+                "SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE"
+            )
+        );
+
+        System.out.println(
+            classifyAccess(
+                "protected",
+                "SUBCLASS_DIFFERENT_PACKAGE_PARENT_TYPE"
+            )
+        );
+    }
+}
