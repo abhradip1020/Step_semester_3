503f39230d135b23014420df1a6a528f6af6b302
abhradip1020 <abhradip1020@gmail.com>
2026-09-12 02:24:00 +0530

    Add Session 4 practice problem 4

---
+++ src/main/java/week4practiceproblems/SrmStudent.java
@@ -0,0 +1,42 @@
+class SrmStudent {
+
+    static String collegeName;
+    static int academicYear;
+
+    String name;
+
+    static {
+        collegeName = "SRM Institute of Science and Technology";
+        academicYear = 2026;
+
+        System.out.println("College info loaded");
+    }
+
+    SrmStudent(String name) {
+        this.name = name;
+    }
+
+    void printConfirmation() {
+        System.out.println("Student record created: " + name);
+    }
+}
+
+public class SrmStudentTest {
+
+    public static void main(String[] args) {
+
+        String[] names = {
+            "Ravi",
+            "Meera",
+            "Karthik",
+            "Divya",
+            "Anitha"
+        };
+
+        for (String name : names) {
+
+            SrmStudent student = new SrmStudent(name);
+            student.printConfirmation();
+        }
+    }
+}
